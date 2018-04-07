6ff1fb23a4f6abbfedee9bc7960516e01d13829d
David Maitland <hello@davidmaitland.me>
2018-04-07 20:57:15 +0100

    Added nrf51_ble400 support, made status configurable

--- src/main.c
+++ src/main.c
@@ -1,13 +1,17 @@
 #include <zephyr.h>
 #include <logging/sys_log.h>
 
+#if defined(CONFIG_FLARE_STATUS)
 #include "status.h"
+#endif
 
 void main(void)
 {
 
     SYS_LOG_INF("Flare 🔥");
 
+    #if defined(CONFIG_FLARE_STATUS)
     status_init();
+    #endif
 
 }
