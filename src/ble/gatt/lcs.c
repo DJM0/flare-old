ddaad4521432b9de158d7e03ae6610ef20b9f64a
David Maitland <hello@davidmaitland.me>
2018-04-30 23:33:05 +0100

    Changed state characteristic to power

--- src/ble/gatt/lcs.c
+++ src/ble/gatt/lcs.c
@@ -9,32 +9,32 @@ struct bt_uuid_128 uuid_lcs = BT_UUID_INIT_128(
     0x00, 0x22, 0x6E, 0x82, 0x09, 0xA3, 0xED, 0xAD,
     0x87, 0x4C, 0xB8, 0x93, 0x2C, 0xA0, 0x34, 0x80);
 
-// State characteristic
+// Power characteristic
 
 // 8034A02C-93B8-4C87-ADED-A309826E2201
-static struct bt_uuid_128 uuid_lcs_state = BT_UUID_INIT_128(
+static struct bt_uuid_128 uuid_lcs_power = BT_UUID_INIT_128(
     0x01, 0x22, 0x6E, 0x82, 0x09, 0xA3, 0xED, 0xAD,
     0x87, 0x4C, 0xB8, 0x93, 0x2C, 0xA0, 0x34, 0x80);
 
-static u8_t state[1];
+static u8_t power[1];
 
-static ssize_t read_state(struct bt_conn *conn, const struct bt_gatt_attr *attr,
+static ssize_t read_power(struct bt_conn *conn, const struct bt_gatt_attr *attr,
     void *buf, u16_t len, u16_t offset)
 {
     SYS_LOG_INF("read");
 
     const char *value = attr->user_data;
     return bt_gatt_attr_read(conn, attr, buf, len, offset, value,
-        sizeof(state));
+        sizeof(power));
 }
 
-static ssize_t write_state(struct bt_conn *conn,
+static ssize_t write_power(struct bt_conn *conn,
     const struct bt_gatt_attr *attr, const void *buf, u16_t len,
     u16_t offset, u8_t flags)
 {
     u8_t *value = attr->user_data;
 
-    if (offset + len > sizeof(state)) {
+    if (offset + len > sizeof(power)) {
         return BT_GATT_ERR(BT_ATT_ERR_INVALID_OFFSET);
     }
 
@@ -81,10 +81,10 @@ static ssize_t write_colour(struct bt_conn *conn,
 
 static struct bt_gatt_attr attrs[] = {
     BT_GATT_PRIMARY_SERVICE(&uuid_lcs),
-    BT_GATT_CHARACTERISTIC(&uuid_lcs_state.uuid,
+    BT_GATT_CHARACTERISTIC(&uuid_lcs_power.uuid,
         BT_GATT_CHRC_READ | BT_GATT_CHRC_NOTIFY | BT_GATT_CHRC_WRITE),
-    BT_GATT_DESCRIPTOR(&uuid_lcs_state.uuid,
-        BT_GATT_PERM_READ | BT_GATT_PERM_WRITE, read_state, write_state, state),
+    BT_GATT_DESCRIPTOR(&uuid_lcs_power.uuid,
+        BT_GATT_PERM_READ | BT_GATT_PERM_WRITE, read_power, write_power, power),
     BT_GATT_CHARACTERISTIC(&uuid_lcs_colour.uuid,
         BT_GATT_CHRC_READ | BT_GATT_CHRC_NOTIFY | BT_GATT_CHRC_WRITE),
     BT_GATT_DESCRIPTOR(&uuid_lcs_colour.uuid,
